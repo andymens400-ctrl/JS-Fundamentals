fdc97fae80aff2b31452f7e71d915c473bc6875a
Andy Darko-Mensah <andymens400@gmail.com>
2025-09-10 09:48:54 +0000

    2-argument.js

---
+++ 2-arguments.js
@@ -0,0 +1,8 @@
+// Arguments
+const argsCounts = process.argv.length - 2;
+
+if (argsCounts === 0) {console.log('No arguments');}
+
+else if (argsCounts === 1) {console.log('Arguement found');}
+
+else{console.log('Arguements found');}
